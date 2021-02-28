2a01128f2d5235247d1c9e108503a9b0576f55df
Aleksandr Molostov <aleksandr.molostov@simbirsoft.com>
2021-02-28 14:00:18 +0400

    lp1 commit 0

--- src/ru/krogot88/Main.java
+++ src/ru/krogot88/Main.java
@@ -3,5 +3,7 @@ package ru.krogot88;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Commit 3");
+
+        System.out.println("lp1 commit 0");
     }
 }
